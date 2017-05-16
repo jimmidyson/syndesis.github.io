777ef1c625440473764a726898b0a4190c2f28f3
kahboom <rachelyordan@gmail.com>
2017-05-16 14:45:40 -0400

    Add sticky sidebar to docs

---
+++ themes/syndesis/js/sticky.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+  $('#sidemenu').affix({
+    offset:{
+      top: 0
+    }
+  });
+
+  console.log($('.main-content').height() - $('.main-content').offset().top);
+
+  /*
+  $('#sidebar').affix({
+    offset:{
+      top: $('#topnav').outerHeight() - 20,
+      bottom: $('footer').outerHeight() + 50
+    }
+  });
+  */
+});
